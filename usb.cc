f44d73956795e70ca4e8019a0095369634185193
Doug Gale <doug16k@gmail.com>
2022-12-09 08:05:32 -0500

    Handle QEMU debug case

--- usb.cc
+++ usb.cc
@@ -3,12 +3,14 @@
 
 void usb_init()
 {
+#ifdef __AVR_ATmega32U4__
   USBCON &= ~(1U << USBE);
   USBCON |= (1U << USBE);
   USBCON &= ~(0x01 << FRZCLK);
   UHWCON |= (0x01 << UVREGE);
   USBCON |= (0x01 << VBUSTE);
   UDCON &= ~(0x01 << DETACH);
+#endif
   // PLLCSR = (((0x01 << PLLTM1) | (0x01 << PLLTM0)) | (0x01 << PLLE));
   // while(!(PLLCSR & (0x01 << PLOCK)));
 }
